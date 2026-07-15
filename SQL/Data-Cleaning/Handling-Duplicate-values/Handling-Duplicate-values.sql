112bb78c9a2eb20e48de68ad7f0f986e3b30a2af
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-07-15 08:10:59 +0530

    Update Handling-Duplicate-values.sql

--- SQL/Data-Cleaning/Handling-Duplicate-values/Handling-Duplicate-values.sql
+++ SQL/Data-Cleaning/Handling-Duplicate-values/Handling-Duplicate-values.sql
@@ -11,3 +11,12 @@ from messy_indian_dataset;
 
 select distinct name
 from messy_indian_dataset;
+
+-- Find the unqiue values using the Rank colummns
+
+SELECT id, name, age, gender, email, phone_number, city, state, purchase_amount, purchase_date 
+FROM (
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY id ORDER BY id) AS 'rank' 
+    FROM messy_indian_dataset
+) AS subtable
+WHERE subtable.rank = 1;
