6e841b1e8df16d13a7d8492af20baad97b78dfea
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-07-16 14:04:08 +0530

    Update Outliers.sql

--- SQL/Data-Cleaning/Outliers/Outliers.sql
+++ SQL/Data-Cleaning/Outliers/Outliers.sql
@@ -5,3 +5,14 @@ use gfg;
 SELECT *, 
        ABS(purchase_amount - AVG(purchase_amount) OVER()) / STDDEV(purchase_amount) OVER() AS 'z_score'
 FROM messy_indian_dataset;
+
+-- Remove the outliers from z score
+
+SELECT * FROM
+(
+    SELECT *, 
+           ABS(purchase_amount - AVG(purchase_amount) OVER()) / STDDEV(purchase_amount) OVER() AS 'z_score'
+    FROM messy_indian_dataset
+) AS sub_table 
+WHERE sub_table.z_score < 3
+;
